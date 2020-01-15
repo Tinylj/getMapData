2aa8b436fc17a70ad60a3482045ac76fe0e8f399
lujin <luj@lhcis.com>
2020-01-15 10:29:18 +0800

    [T4157]feature
    feature for route.js

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ##调试
 
-http://127.0.0.1:7001/podevent/getQRCode?compCode=7933B1B794
+http://127.0.0.1:7001/map/log?compCode=7933B1B794
--- app/router.js
+++ app/router.js
@@ -1,5 +1,4 @@
 module.exports = app => {
     const { router, controller } = app;
-    router.get('/podevent/log', controller.podevent.log);
-    router.get('/podevent/getQRCode', controller.podevent.getQRCode);
+    router.get('/map/log', controller.map.log);
 };
